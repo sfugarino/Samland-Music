61d90102f0f621ecfc747a5ee3b46b31fca02fba
Samuel Fugarino <sfugarino@msn.com>
2020-09-07 11:35:10 -0400

    deleted:    .browserslistrc

--- src/app/services/album.ts
+++
@@ -1,7 +0,0 @@
-import { Song } from './song';
-
-export class Album {
-    name = '';
-    releaseDate = '';
-    imageUrl = '';
-}
--- src/app/services/artist.ts
+++
@@ -1,9 +0,0 @@
-import { Album } from './album';
-export class Artist {
-    id = 0;
-    name = '';
-    genre = '';
-    imageUrl = '';
-    members: string[] | undefined;
-    albums: Album[] | undefined;
-}
